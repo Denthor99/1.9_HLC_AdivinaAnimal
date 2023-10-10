836b745fa3f1b30fd503691240da7fe042b64ea0
Denthor99 <darancuga@hotmail.com>
2023-10-10 13:15:33 +0200

    Planteamiento de la actividad + codificar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Variables que usaremos en el ejercicio
+  esCorrecto: boolean = false;
+  animalSecreto: string = "";
+  animalUsuario: string = "";
+  mensajeParaUser: string = "";
 
-  constructor() {}
+  constructor() {
+    // El método animalAleatorio se ejecutará al iniciar el programa
+    this.animalAleatorio();
+  }
+
+  comprobarAnimal(){
+    console.log("Click en el botón");
+    console.log("animalUsuario= "+this.animalUsuario);
+    if (this.animalSecreto == this.animalUsuario){
+      console.log("Enhorabuena, has acertado");
+      this.mensajeParaUser = "Enhorabuena, el animal secreto es " + this.animalSecreto;
+      this.esCorrecto = true;
+    } else {
+      console.log("CASI!!, Prueba otra vez");
+      this.mensajeParaUser = "¡¡CASI!!, pruebe otra vez"
+    }
+  }
+
+  animalAleatorio(){
+    // Crearemos un array de cadenas, donde introduciremos una lista de animales
+    let listaAnimales: string[] = ["León","Gato","Camello", "Loro", "Perro","Dromedario","Gorrión","Elefante"];
+
+    // Añadimos a la variable "animalSecreto" un valor aleatorio del array. Para ello usaremos la funcion math.random  para generar un valor aleatorio que coincida con alguno de los indices
+    this.animalSecreto = listaAnimales[Math.floor(Math.random()*listaAnimales.length)];
+    console.log("AnimalSecreto= "+this.animalSecreto);
+  }
+
+  reiniciar(){  
+    this.animalAleatorio();
+    this.esCorrecto = false;
+    this.animalUsuario = "";
+    this.mensajeParaUser = "";  
+  }
 
 }
